89126283d5556751614be7eb39c9d93659763109
225j2005-a11y <yutongj24@gmail.com>
2026-01-10 15:16:24 +0900

    添加 sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'jia-timebox-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// 安装：缓存基础资源
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// 激活：清理旧缓存
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(cacheNames.map((cn) => (cn !== CACHE_NAME ? caches.delete(cn) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 拦截请求：优先缓存，回退网络
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
